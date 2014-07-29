2da1a57c853ce3bd2af8be0bc96ee6a1733ddcc6
Will Lovett <willlovett@gmail.com>
2014-07-29 11:06:58 +0200

    Added xdot

---
+++ recipes/xdot.rb
@@ -0,0 +1,15 @@
+directory "/opt/applications" do
+	owner "root"
+	group "root"
+	mode 00775
+	action: create
+end
+
+git "/opt/applications/xdot.py" do
+	repository "https://github.com/jrfonseca/xdot.py"
+	action :sync
+end
+
+link "/usr/local/bin/xdot.py" do
+	to "/opt/applications/xdot.py/xdot.py"
+end
